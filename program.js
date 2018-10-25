ecf95ab59152b55bc57fe4a28b6bf6a27011de4e
Slawomir Masluk <slawomirmasluk@gmail.com>
2018-10-25 16:49:11 +0200

    Add program.js

--- program.js
+++ program.js
@@ -1,6 +1,5 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function () {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
